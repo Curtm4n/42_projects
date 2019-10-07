b52ab9b0d915ac6951e09d349077e6f2b74bbf65
Curtis DAPURIFICACAO <cdapurif@e2r9p17.42.fr>
2019-10-07 18:43:58 +0200

    Ajout de fonctions de la libft

---
+++ ft_memcpy.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cdapurif <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/07 17:33:52 by cdapurif          #+#    #+#             */
+/*   Updated: 2019/10/07 18:21:03 by cdapurif         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+		i++;
+	}
+	return (dest);
+}
