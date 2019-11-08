bcf48b6008f631aac877776b96aff071dc827342
Curtis DAPURIFICACAO <cdapurif@e3r9p15.42.fr>
2019-11-08 16:58:15 +0100

    Debut du projet ft_printf

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cdapurif <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/08 16:52:55 by cdapurif          #+#    #+#             */
+/*   Updated: 2019/11/08 16:56:12 by cdapurif         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+
+int		ft_printf(const char *str, ...);
+
+#endif
