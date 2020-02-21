c583821f77b01398617e02571459112aae4b2db8
Curtis DAPURIFICACAO <cdapurif@e1r7p9.42.fr>
2020-02-21 18:45:47 +0100

    Deuxieme domaine fonctionnel, mariadb configure ainsi que php et phpmyadmin installe et configure

--- ft_server/srcs/script/launch_commands.sh
+++ ft_server/srcs/script/launch_commands.sh
@@ -1,1 +1,5 @@
+/etc/init.d/mysql start
+/etc/init.d/php7.3-fpm start
+mariadb < /usr/share/phpmyadmin/sql/create_tables.sql
+mysql -e "GRANT SELECT, INSERT, UPDATE, DELETE ON phpmyadmin.* TO 'pma'@'localhost' IDENTIFIED BY 'hello'"
 service nginx start
