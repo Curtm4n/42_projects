6d5fe48f0148004e9e55deb7eb50d38b405e050c
Curtis Da Purificaçao <curtman@MacBook-Pro-de-Curtis.local>
2020-02-23 22:09:49 +0100

    phpmyadmin is now set up

--- ft_server/srcs/script/launch_commands.sh
+++ ft_server/srcs/script/launch_commands.sh
@@ -1,5 +1,7 @@
 /etc/init.d/mysql start
 /etc/init.d/php7.3-fpm start
-mariadb < /usr/share/phpmyadmin/sql/create_tables.sql
+mariadb < /var/www/localhost/phpmyadmin/sql/create_tables.sql
 mysql -e "GRANT SELECT, INSERT, UPDATE, DELETE ON phpmyadmin.* TO 'pma'@'localhost' IDENTIFIED BY 'hello'"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'curtis'@'localhost' IDENTIFIED BY 'hello' WITH GRANT OPTION"
+mysql -e "FLUSH PRIVILEGES"
 service nginx start
